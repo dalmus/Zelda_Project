585eb9e3c73d04c3ed34a3788c9c0bfe91397ee8
esau jimenez <ovan20081@outlook.com>
2017-02-07 12:31:44 -0800

    update

--- js/script.js
+++ js/script.js
@@ -3,30 +3,3 @@ $(document).ready(function() {
     $(".loader").fadeOut(1500);
   },27000);
 });
-//
-// rupeeGrid = [
-//     [0, 0, 0, 0],
-//     [0, 0, 0, 0],
-//     [0, 0, 0, 0],
-//     [0, 0, 0, 0]
-// ];
-//
-// preload: function(){
-//     this.game.load.image('blue', 'image/Blue.png');
-//     this.game.load.image('red', 'image/Red.png');
-//     this.game.load.image('purple','image/Purple.png');
-// },
-//
-// document.getElementsByClassName('rupee_box').addEventListener('click', function(){
-//   random(){
-//
-//   }
-//
-//   };
-var rupeeColors = [[255, 128, 128],
-                     [128, 255, 128],
-                     [128, 128, 255],
-                     [255, 255, 128],
-                     [255, 128, 255],
-                     [128, 255, 255],
-                     [255, 255, 255]];
