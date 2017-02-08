343cd00cd13121eeb0444143fc4dd9f9c3dbf779
esau jimenez <ovan20081@outlook.com>
2017-02-07 16:48:17 -0800

    update

--- js/script.js
+++ js/script.js
@@ -3,3 +3,47 @@ $(document).ready(function() {
     $(".loader").fadeOut(1500);
   },27000);
 });
+//
+
+      var imgCount = 3;
+       var images = new Array
+           images[0] = "img/green.png",
+           images[1] = "img/blue.png",
+           images[2] = "img/red.png",
+           images[3] = "img/purple.png"
+
+for (var i = 0; i < 17; i++) {
+  var randomCount = Math.round(Math.random() * 3)
+  document.getElementsByClassName("rupee_box")[i].style.backgroundImage = "url(" + images[randomCount] + ")";
+}
+
+
+//----------
+
+
+
+
+
+
+
+
+
+// <div class="box rubyRed" row="0" col="0"></div>
+// <div class="box rubyGreen" row="0" col="0"></div>
+//
+// var toSwap = []
+// $('.box').click(function(){
+//     var row = $(this).attr('row')
+//     var col = $(this).attr('col')
+//     toSwap.push({
+//         box: $(this),
+//         row: row,
+//         col: col
+//     })
+//     if (toSwap.length === 2) {
+//         if(toSwap[0].row === toSwap[1].row && Math.abs(toSwap[0].col - toSwap[1].col) == 1) {
+//             toSwap[0].toggleClass('rubyRed rubyGreen')
+//             toSwap[1].toggleClass('rubyRed rubyGreen')
+//         }
+//     }
+// })
